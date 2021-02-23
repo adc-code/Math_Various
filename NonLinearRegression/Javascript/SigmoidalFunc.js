8faeed7db61ef160e3db20cdf43701546acfe7ce
adc-code <61217881+adc-code@users.noreply.github.com>
2021-02-23 15:51:28 -0500

    Add files via upload

--- NonLinearRegression/Javascript/SigmoidalFunc.js
+++ NonLinearRegression/Javascript/SigmoidalFunc.js
@@ -3,13 +3,13 @@ function D3App ()
     // SVG Width, height, and some added spacing
     var margin = { top: 10, right: 30, bottom: 20, left: 40 };
     var width  = 500 - margin.left - margin.right;
-    var height = 300 - margin.top  - margin.bottom;
+    var height = 350 - margin.top  - margin.bottom;
 
     var _w = 1;
     var _b = 0;
 
-    var snapToInt = false;
-    var visCurve  = false;
+    var _region = 0;
+    var _snapToInt = false;
     var pointList = [];
 
     // Create SVG element
@@ -88,6 +88,14 @@ function D3App ()
                      .x (function(d) { return d.x;  })
                      .y (function(d) { return d.y;  });
 
+    var botLine = d3.line ()
+                     .x (function(d) { return d.x;  })
+                     .y (function(d) { return d.y;  });
+
+    var topLine = d3.line ()
+                     .x (function(d) { return d.x;  })
+                     .y (function(d) { return d.y;  });
+
     var solutionLine = d3.line ()
                          .x (function(d) { return d.x;  })
                          .y (function(d) { return d.y;  })
@@ -95,16 +103,37 @@ function D3App ()
 
     // Add the query lines to the svg
     svg.append ('path')
-       .datum ( [ {x: xScale.domain()[0], y: yScale(0)}, {x: xScale.domain()[1], y: yScale(0)} ] )
+       .datum ( [ {x: xScale.range()[0], y: yScale(0.5)}, {x: xScale.range()[1], y: yScale(0.5)} ] )
        .attr ('class', 'queryLine') 
        .attr ('id', 'horizLine')
        .attr ('d', horizLine);
 
     svg.append ('path')
-       .datum ( [ {x: xScale(0), y: yScale.domain()[0]}, {x: xScale(0), y: yScale.domain()[1]} ] )
+       .datum ( [ {x: xScale(0), y: yScale.range()[0]}, {x: xScale(0), y: yScale.range()[1]} ] )
        .attr ('class', 'queryLine') 
        .attr ('id', 'vertLine')
        .attr ('d', vertLine);
+
+    svg.append ('path')
+       .datum ( [ {x: xScale.range()[0], y: yScale(0)}, {x: xScale.range()[1], y: yScale(0)} ] )
+       .attr ('class', 'pointLine') 
+       .attr ('id', 'botLine')
+       .attr ('d', botLine);
+
+    svg.append ('path')
+       .datum ( [ {x: xScale.range()[0], y: yScale(1)}, {x: xScale.range()[1], y: yScale(1)} ] )
+       .attr ('class', 'pointLine') 
+       .attr ('id', 'topLine')
+       .attr ('d', topLine);
+
+    // Make an empty rectangle to handle mouse move events...
+    svg.append ('rect')
+       .attr ('width', width)
+       .attr ('height', height)
+       .style ('fill', 'none')
+       .style ('pointer-events', 'all')
+       .on ('mousemove', mouseMove)
+       .on ('click', mouseClick);
 
     var sigmoidCurve = computeSigmoidalFunc (_w, _b);
 
@@ -136,6 +165,39 @@ function D3App ()
         }
 
         return results;
+    }
+
+
+    // 
+    // drawCircle: used to draw a point on the screen
+    //
+    function drawCircle (x, y, size)
+    {
+        svg.append ('circle')
+           .attr ('class', 'point')
+           .attr ('cx', x)
+           .attr ('cy', y)
+           .attr ('r', size)
+           .transition ()
+           .duration (500)
+           .ease (d3.easeBounceOut)
+           .tween ('moveCirc', circMover);
+        
+    }
+
+    var circMover = function ()
+    {
+        var startY = d3.select (this).attr ('cy');
+
+        var endY   = yScale (0);
+        if (_region == 1)
+            endY = yScale (1);          
+
+        var interpolator = d3.interpolate (startY, endY);
+        return function (t)
+        {
+            d3.select (this).attr ('cy', interpolator(t));
+        }
     }
 
 
@@ -179,7 +241,12 @@ function D3App ()
 
         // console.log (selectedX, selectedY);
 
-        if (snapToInt)
+        if (yScale.invert (selectedY) < 0.5)
+            _region = 0;
+        else
+            _region = 1;
+
+        if (_snapToInt)
         {
             selectedX = xScale (math.round (xScale.invert (selectedX)) );
             selectedY = yScale (math.round (yScale.invert (selectedY)) );
@@ -192,7 +259,119 @@ function D3App ()
         locData = [ { x: selectedX, y: yScale.range()[0] }, 
                     { x: selectedX, y: yScale.range()[1] } ];
         svg.select ('#vertLine').attr ('d', vertLine (locData));
+
+        if (_region == 0)
+        {
+            d3.select ('#botLine').node().classList = ['pointLineSel'];
+            d3.select ('#topLine').node().classList = ['pointLine'];
+        }
+        else
+        {
+            d3.select ('#botLine').node().classList = ['pointLine'];
+            d3.select ('#topLine').node().classList = ['pointLineSel'];
+        }
     }
+
+
+    //
+    // mousemove callback function used for the query line
+    //
+    function mouseClick ()
+    {
+        var selectedX = d3.mouse(this)[0];
+        var selectedY = d3.mouse(this)[1];
+
+        // console.log (selectedX, selectedY);
+
+        if (_snapToInt)
+        {
+            selectedX = xScale (math.round (xScale.invert (selectedX)) );
+            selectedY = yScale (math.round (yScale.invert (selectedY)) );
+        }
+
+        drawCircle (selectedX, selectedY, 4);
+ 
+        pointList.push ( [selectedX, selectedY] );
+    }  
+
+
+    // 
+    // callback to handle mouseEnter on a button 
+    //
+    d3.selectAll ('.button').on ('mouseenter', function ()
+    {
+        //console.log ('On button mouseenter CB');
+
+        // enter... darken the button
+        var id = d3.select(this).node().getAttribute ('id');
+
+        if (id == 'RemoveBtn')
+        {
+            d3.select(this).node().style.backgroundColor = '#cc0000';
+            d3.select(this).node().style.color           = '#ffffff';
+        }
+        else if (id == 'SnapToIntBtn')
+        {
+            d3.select(this).node().style.backgroundColor = '#00eaff';
+        }
+
+    } ); 
+
+
+    // 
+    // callback to handle mouseLeave on a button 
+    //
+    d3.selectAll ('.button').on ('mouseleave', function ()
+    {
+        // console.log ('On button mouseleave CB');
+
+        // leave... put button to previous state
+        var id = d3.select(this).node().getAttribute ('id');
+
+        if (id == 'SnapToIntBtn' && _snapToInt == true)
+        {
+            d3.select(this).node().style.backgroundColor = '#00aabb';
+            d3.select(this).node().style.color           = '#ffffff';
+        }
+        else
+        {
+            d3.select(this).node().style.backgroundColor = '#cccccc';
+            d3.select(this).node().style.color           = '#000000';
+        }
+
+    } );
+
+
+    // 
+    // callback to handle click on the snap to integer button
+    //
+    d3.select ('#SnapToIntBtn').on ('click', function ()
+    {
+        _snapToInt = !_snapToInt;
+
+        if (_snapToInt)
+        {
+            d3.select(this).node().style.backgroundColor = '#00aabb';
+            d3.select(this).node().style.color           = '#ffffff';
+        }
+        else
+        {
+            d3.select(this).node().style.backgroundColor = '#cccccc';
+            d3.select(this).node().style.color           = '#000000';
+        }
+
+    } );
+
+
+    // 
+    // callback to handle click on the reseet button
+    //
+    d3.select ('#RemoveBtn').on ('click', function ()
+    {
+        // remove points...
+        pointList = [];
+        svg.selectAll ('circle').remove()
+    } );
 
 }
 
